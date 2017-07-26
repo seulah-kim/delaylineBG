4ca21afd83bbeecf3d984037a89f9fbe8e2fe61a
Seul Ah Kim <seulah_kim@g.harvard.edu>
2017-07-26 09:30:18 -0400

    introduced input parser object
    simulation takes much longer.

--- BGdelayline.m
+++ BGdelayline.m
@@ -1,76 +1,146 @@
-%% BG delay line model
-clear 
-clc
-r = 10; % convergence ratio between layers
-n = 100; % number of str neurons
+%% BGdelayline3.M
+%This is the main code for simulating BG delay line model
+function [Vm_gp, Vm_snr, Vm_str, Isyn_gp_out, Isyn_snr_out] = BGdelayline(varargin)
 
-%%Initial Conditions
-Vrest = -70;  % mV
-Cm = 1; % Membrane capacitance
-g_in = 0.3; % inhibitory synaptic condl (mV)
-E_snr = -85; % SNr-synaptic reversal potenuctance
-g_ex = 1; % excitatory synaptic conductance (optional)
-tau_cell = 5;   % cell decay constant (ms)
-tau_syn = 5;  % synaptic decay constant (ms)
-E_gp = -85; % GP-synaptic reversal potentiatial (mV)
-spk_count_str = zeros(n,1);
-spk_count_gp = zeros(n./r,1);
-spk_count_snr = zeros(n./r.^2,1);
-g_unitary = 1;  % conductance of each synapse (mS)
-prob_syn = 0.8; % probability of successful synaptic transmission
+tic
+close all
 
-%% Feedforward inhibition
-% s = rng;
-g_i0 = g_in*ones((n/r+n/r.^2),1);
-Vm_i0 = Vrest*ones((n+n/r+n/r.^2),1);
-%Simulation time
-t_span = 0:0.001:10;
+p = inputParser;	% construct input parser object
 
-%Probablistic synapse
-% rng(1)  % for repeatable simulations-- set random number generator with sequence #1
-% probSyn_str = rand(length(t_span),n)>prob_syn; % flip a coin: success (1) or failure (0)
-% probSyn_gp = rand(length(t_span),n./r)>prob_syn; 
-y0 = [g_i0;Vm_i0];
-options = odeset('Events',@spikeevents,'RelTol',1e-4,'AbsTol',1e-7);
-tout = [];
-g_str2gp = [];
-g_gp2snr = [];
-Vm_str = [];
-Vm_gp = [];
-Vm_snr = [];
-for i = 1:100
-[T,Y,te,ye,ie] = ode45(@(t,x)Vm_diffeq(t,x,n,r),t_span,y0,options);
+%%Network size
+p.addParameter('r', 10); % convergence ratio between layers
+p.addParameter('n', 100); % number of str neurons
 
-nt = length(T);
-tout = [tout; T(1:nt)];
-g_str2gp = [g_str2gp; Y(:,1:n/r)];
-g_gp2snr = [g_gp2snr; Y(:,(n/r+1):(n/r+n/r.^2))];
-Vm_str = [Vm_str; Y(:,(n/r+n/r.^2+1):(n+n/r+n/r.^2))];
-Vm_gp = [Vm_gp; Y(:,(n+n/r+n/r.^2+1):(n+2*n/r+n/r.^2))];
-Vm_snr = [Vm_snr; Y(:,(n+2*n/r+n/r.^2+1):size(Y,2))];
+%%Simulation time
+dt = 0.001; %(s)
+t_span = 0:dt:20;
 
-y0([Y(nt,:)>=-64]')= Vrest;
-t_span = t_span(length(tout)+1:end);
-if length(t_span)<=1
-    break
+%%Define Constants
+Cm = 0.1;        % Cell capacitance (nF)
+Erev_i = -85;   % Synaptic reversal potential(mV)
+I_const = 0.1;   % General scale for constant excitatory input(pA)
+Vrest = -70;    % Resting potential(mV)
+Vpeak = 15;     % Peak potential(mV)
+V_thres = -64;  % Threshold voltage(mV)
+%Synaptic
+p.addParameter('tau_syn',5); % synaptic decay constant (ms)
+p.addParameter('prob_syn', 0.95); % probability of successful synaptic transmission
+g_uni = 1 ; % conductance of a single synapse
+
+%%Nonlinear function to filter synaptic input (sigmoid function)
+%values for GP 
+p.addParameter('k_gp',1000);    % steepness of logistic function
+p.addParameter('xd_gp', 5);    % x location of midpoint of logistic function
+p.addParameter('L_gp', 40);     % max value of curve
+%values for SNr
+p.addParameter('k_snr', 1000);    % steepness of logistic function
+p.addParameter('xd_snr' ,6.5);   % x location of midpoint of logistic function
+p.addParameter('L_snr', 40);     % max value of curve
+
+%%Input current to Str layer
+randNum = round((20-10).*rand(1,1)+10);
+randNum = 5;	% timing of Stimulation
+tStim = [randNum:dt:randNum+0.5]; % (s) 
+% only the Str cell #5 will receive input
+p.addParameter('stimCellsPer',100) 
+
+p.parse(varargin{:});	% parse and validate input arguments
+
+% Assign variables from pased input arguments
+r = p.Results.r;
+n = p.Results.n;
+tau_syn = p.Results.tau_syn;
+prob_syn = p.Results.prob_syn;
+k_gp = p.Results.k_gp;
+xd_gp = p.Results.xd_gp;
+L_gp = p.Results.L_gp;
+k_snr = p.Results.k_snr;
+xd_snr = p.Results.xd_snr;
+L_snr = p.Results.L_snr;
+stimCellsPer = p.Results.stimCellsPer;
+
+%%Initialize variables
+%Cellular
+tau_cell_str = ones(n,1);   % cell decay constant (ms)
+tau_cell_gp = ones(n/r,1);   % cell decay constant (ms)
+tau_cell_snr = ones(n/r.^2,1);   % cell decay constant (ms)
+%homogeneous conductance
+g_str2gp = 0.05*ones(n/r,1);    % (nS)
+g_gp2snr = 0.3*ones(n/r.^2,1);
+%heterogeneous Vstart
+Vm_str = Vrest+2*randn(n,1);  % (mV)
+Vm_gp = Vrest+2*randn(n/r,1);
+Vm_snr = Vrest+2*randn(n/r.^2,1);
+delta_str = zeros(n,1); % binary
+delta_gp = zeros(n/r,1);
+delta_snr = zeros(n/r.^2,1);
+Isyn_gp_out =[];
+Isyn_snr_out =[];
+%neuron filter for synaptic inputs
+% for GP
+syms synNonlin(x)
+synNonlin(x)= piecewise(x<=(xd_gp-2/k_gp),0,(xd_gp-2/k_gp)<x<=xd_gp, L_gp*k_gp/4*(x-xd_gp)+L_gp/2,x>xd_gp,L_gp./(1+exp(-k_gp*(x-xd_gp))));    % check if this is in the working range
+% for SNr
+syms synNonlin_snr(x)
+synNonlin_snr(x) =piecewise(x<=(xd_snr-2/k_snr),0,(xd_snr-2/k_snr)<x<=xd_snr, L_snr*k_snr/4*(x-xd_snr)+L_snr/2,x>xd_snr,L_snr./(1+exp(-k_snr*(x-xd_snr))));   % check if this is in the working range
+% external input to Str
+Iext_str = zeros(n,length(t_span));
+stimCells=datasample(1:n,n*stimCellsPer/100,'Replace',false);	% Random sample of str cells for receiving stimulus, defined by percentage 
+Iext_str(stimCells,ismember(t_span,tStim)) = 50*I_const;  % constant input to striatum ( 0.6~0.61Iconst for relatively quiet str cells) (in pA)
+
+
+for t = 1:length(t_span)
+%Striatum
+%Iext_str = I_const*ones(n,1);  % constant input to striatum ( 0.6~0.61Iconst for relatively quiet str cells) (in pA)
+dVm_str = (-1*(Vm_str(:,t)-Vrest*ones(n,1))./tau_cell_str + Iext_str(:,t)/Cm)*dt;   % no nonlinear function here!
+
+%GP
+synSuccess_str2gp = double(rand(n,n/r)<prob_syn);
+Isyn_gp = g_str2gp(:,t).*(Vm_gp(:,t)-Erev_i*ones(n/r,1));  % (pA)
+Iext_gp = 50*I_const*ones(n/r,1);
+dg_str2gp = (-g_str2gp(:,t)./tau_syn + transpose(delta_str'*synSuccess_str2gp*g_uni))*dt;
+dVm_gp = (-(Vm_gp(:,t)-Vrest*ones(n/r,1))./tau_cell_gp - synNonlin(Isyn_gp)/Cm+Iext_gp/Cm)*dt;
+% dVm_gp = (-(Vm_gp(:,t)-Vrest*ones(n/r,1))./tau_cell_gp +Iext_gp/Cm)*dt; %
+
+%SNr
+synSuccess_gp2snr = double(rand(n/r,n/r.^2)<prob_syn);
+Isyn_snr = g_gp2snr(:,t).*(Vm_snr(:,t)-Erev_i.*ones(n/r.^2,1));
+Iext_snr = 40*I_const*ones(n/r.^2,1);
+dg_gp2snr = (-g_gp2snr(:,t)./tau_syn + transpose(delta_gp'*synSuccess_gp2snr*g_uni))*dt;
+dVm_snr = (-(Vm_snr(:,t)-Vrest*ones(n/r.^2,1))./tau_cell_snr - synNonlin_snr(Isyn_snr)/Cm+Iext_snr/Cm)*dt;
+
+%Update conductances
+g_str2gp(:,t+1) = g_str2gp(:,t)+dg_str2gp;
+g_gp2snr(:,t+1) = g_gp2snr(:,t)+dg_gp2snr;
+
+%Check for spikes in the cells
+delta_str = double((Vm_str(:,t)+dVm_str)>=V_thres);
+delta_gp = double((Vm_gp(:,t)+dVm_gp)>=V_thres);
+delta_snr = double((Vm_snr(:,t)+dVm_snr)>=V_thres);
+
+%Update spike patterns
+%Str
+Vm_str(:,t+1) = Vm_str(:,t)+dVm_str;
+if sum(delta_str)>=1
+    Vm_str((Vm_str(:,t)+dVm_str)>=V_thres,t) = Vpeak;
+    Vm_str((Vm_str(:,t)+dVm_str)>=V_thres,t+1) = Vrest;
 end
+%GP
+Vm_gp(:,t+1) = Vm_gp(:,t)+dVm_gp;
+if sum(delta_gp)>=1
+    Vm_gp((Vm_gp(:,t)+dVm_gp)>=V_thres,t) = Vpeak;
+    Vm_gp((Vm_gp(:,t)+dVm_gp)>=V_thres,t+1) = Vrest;
 end
-%%
-figure
-subplot(2,1,1)
-% plot(TOUT,g_str2gp,'r')
-hold on
-plot(tout,g_gp2snr,'g')
-legend('g_{str2gp}','g_{gp2snr}')
-ylabel('conductance (mS?)')
+%SNr
+Vm_snr(:,t+1) = Vm_snr(:,t)+dVm_snr;
+if sum(delta_snr)>=1
+    Vm_snr((Vm_snr(:,t)+dVm_snr)>=V_thres,t) = Vpeak;
+    Vm_snr((Vm_snr(:,t)+dVm_snr)>=V_thres,t+1) = Vrest;
+end 
 
-subplot(2,1,2)
-% plot(TOUT,Vm_str,'k')
-hold on
-plot(tout,Vm_gp,'g')
-% plot(TOUT,Vm_snr,'r')
-legend('Vm_{str}','Vm_{gp}','Vm_{snr}')
-ylabel('membrane Voltage (mV)')
-xlabel('Time (s)')
-%%total synaptic conductance
-% g_str=sum(spk_count_str.*probSyn_str      % how to advance through probSyn_str??
+%Save variables 
+Isyn_gp_out = [Isyn_gp_out,Isyn_gp];
+Isyn_snr_out = [Isyn_snr_out,Isyn_snr];
+end
+toc
+end
