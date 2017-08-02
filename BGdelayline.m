abbb41894b1b74845879ff0bb44a24d83dd20cfb
Seul Ah Kim <seulah_kim@g.harvard.edu>
2017-08-02 17:01:45 -0400

    Jupyter notebook title change
    Additional update: Started exploring the effect of GP firing rate on Str-to-SNr signaling delay.

--- BGdelayline.m
+++ BGdelayline.m
@@ -27,36 +27,22 @@ p.addParameter('tau_syn',5); % synaptic decay constant (ms)
 p.addParameter('prob_syn', 0.35); % probability of successful synaptic transmission
 g_uni = 100 ; % conductance of a single synapse
 
-%%Nonlinear function to filter synaptic input (sigmoid function)
-%values for GP 
-%p.addParameter('k_gp',1000);    % steepness of logistic function
-%p.addParameter('xd_gp', 5);    % x location of midpoint of logistic function
-%p.addParameter('L_gp', 40);     % max value of curve
-%%values for SNr
-%p.addParameter('k_snr', 1000);    % steepness of logistic function
-%p.addParameter('xd_snr' ,6.5);   % x location of midpoint of logistic function
-%p.addParameter('L_snr', 40);     % max value of curve
-
 %%Input current to Str layer
 randNum = round((20-10).*rand(1,1)+10);
 randNum = 5;	% timing of Stimulation
 tStim = [randNum:dt:randNum+0.5]; % (s) 
+IextRatio_gpsnr = 10/3;
 % only the Str cell #5 will receive input
 p.addParameter('stimCellsPer',100) 
 
-p.parse(varargin{:});	% parse and validate input arguments
+%% Parse and validate input arguments
+p.parse(varargin{:}); 
 
 % Assign variables from pased input arguments
 r = p.Results.r;
 n = p.Results.n;
 tau_syn = p.Results.tau_syn;
 prob_syn = p.Results.prob_syn;
-%k_gp = p.Results.k_gp;
-%xd_gp = p.Results.xd_gp;
-%L_gp = p.Results.L_gp;
-%k_snr = p.Results.k_snr;
-%xd_snr = p.Results.xd_snr;
-%L_snr = p.Results.L_snr;
 stimCellsPer = p.Results.stimCellsPer;
 
 %%Initialize variables
@@ -65,7 +51,7 @@ tau_cell_str = ones(n,1);   % cell decay constant (ms)
 tau_cell_gp = ones(n/r,1);   % cell decay constant (ms)
 tau_cell_snr = ones(n/r.^2,1);   % cell decay constant (ms)
 %homogeneous conductance
-g_str2gp = 0.05*ones(n/r,1);    % (nS)
+g_str2gp = 0.05*ones(n/r,1);    % (nS)	
 g_gp2snr = 0.3*ones(n/r.^2,1);
 %heterogeneous Vstart
 Vm_str = Vrest+2*randn(n,1);  % (mV)
@@ -76,19 +62,11 @@ delta_gp = zeros(n/r,1);
 delta_snr = zeros(n/r.^2,1);
 Isyn_gp_out =[];
 Isyn_snr_out =[];
-%neuron filter for synaptic inputs
-% for GP
-%syms synNonlin(x)
-%synNonlin(x)= piecewise(x<=(xd_gp-2/k_gp),0,(xd_gp-2/k_gp)<x<=xd_gp, L_gp*k_gp/4*(x-xd_gp)+L_gp/2,x>xd_gp,L_gp./(1+exp(-k_gp*(x-xd_gp))));    % check if this is in the working range
-% for SNr
-%syms synNonlin_snr(x)
-%synNonlin_snr(x) =piecewise(x<=(xd_snr-2/k_snr),0,(xd_snr-2/k_snr)<x<=xd_snr, L_snr*k_snr/4*(x-xd_snr)+L_snr/2,x>xd_snr,L_snr./(1+exp(-k_snr*(x-xd_snr))));   % check if this is in the working range
-% external input to Str
 Iext_str = zeros(n,length(t_span));
 stimCells=datasample(1:n,n*stimCellsPer/100,'Replace',false);	% Random sample of str cells for receiving stimulus, defined by percentage 
 Iext_str(stimCells,ismember(t_span,tStim)) = 50*I_const;  % constant input to striatum ( 0.6~0.61Iconst for relatively quiet str cells) (in pA)
 
-
+%%Simulation
 for t = 1:length(t_span)
 %Striatum
 %Iext_str = I_const*ones(n,1);  % constant input to striatum ( 0.6~0.61Iconst for relatively quiet str cells) (in pA)
@@ -97,7 +75,7 @@ dVm_str = (-1*(Vm_str(:,t)-Vrest*ones(n,1))./tau_cell_str + Iext_str(:,t)/Cm)*dt
 %GP
 synSuccess_str2gp = double(rand(n,n/r)<prob_syn);
 Isyn_gp = g_str2gp(:,t).*(Vm_gp(:,t)-Erev_i*ones(n/r,1));  % (pA)
-Iext_gp = 25*I_const*ones(n/r,1);
+Iext_gp = 50*I_const*ones(n/r,1);
 dg_str2gp = (-g_str2gp(:,t)./tau_syn + transpose(delta_str'*synSuccess_str2gp*g_uni/n))*dt;
 dVm_gp = (-(Vm_gp(:,t)-Vrest*ones(n/r,1))./tau_cell_gp - Isyn_gp/Cm+Iext_gp/Cm)*dt;
 % dVm_gp = (-(Vm_gp(:,t)-Vrest*ones(n/r,1))./tau_cell_gp +Iext_gp/Cm)*dt; %
@@ -105,7 +83,7 @@ dVm_gp = (-(Vm_gp(:,t)-Vrest*ones(n/r,1))./tau_cell_gp - Isyn_gp/Cm+Iext_gp/Cm)*
 %SNr
 synSuccess_gp2snr = double(rand(n/r,n/r.^2)<prob_syn);
 Isyn_snr = g_gp2snr(:,t).*(Vm_snr(:,t)-Erev_i.*ones(n/r.^2,1));
-Iext_snr = 125*I_const*ones(n/r.^2,1);
+Iext_snr = round(50*IextRatio_gpsnr,2)*I_const*ones(n/r.^2,1);
 dg_gp2snr = (-g_gp2snr(:,t)./tau_syn + transpose(delta_gp'*synSuccess_gp2snr*g_uni/(n/r)))*dt;
 dVm_snr = (-(Vm_snr(:,t)-Vrest*ones(n/r.^2,1))./tau_cell_snr - Isyn_snr/Cm+Iext_snr/Cm)*dt;
 
@@ -142,5 +120,8 @@ end
 Isyn_gp_out = [Isyn_gp_out,Isyn_gp];
 Isyn_snr_out = [Isyn_snr_out,Isyn_snr];
 end
+spike_str = Vm_str==Vpeak;
+
 toc
+
 end
---
+++ plotRaster.m
@@ -0,0 +1,41 @@
+function plotRaster(traceM)
+
+%Converts to a binary matrix indicating spike occurence
+SpikeM = traceM==15;
+%x and y coordinates
+[neuron,time] = find(SpikeM);
+
+%To avoid indexing problem of "find" function for an array
+if size(SpikeM,1)==1
+    time = find(SpikeM)';
+    neuron = ones(length(time),1);
+end
+
+neuron = neuron';
+time = time';
+lineLength = 1/2;
+%Creating x and y arrays for vertical lines
+xPoints = [ time ;
+            time ;
+            NaN(size(time)) ];
+yPoints = [ neuron - lineLength ;
+            neuron + lineLength ;
+            NaN(size(neuron)) ];
+xPoints = xPoints(:);
+yPoints = yPoints(:);
+t_plot = xPoints*0.001;
+plot(t_plot,yPoints,'k');
+      
+set(gca,'YDir','reverse');
+set(gca,'TickDir','out') 
+xlabel('Time (s)');
+ylabel('Neuron');
+xlim([0 (size(traceM,2)+1)*0.001]);
+ylim([0 size(traceM,1)+1]);
+if size(SpikeM,1) == 1
+    set(gca,'YTick', [0 1])                        % don't draw y-axis ticks
+    set(gca,'PlotBoxAspectRatio',[1 0.05 1])    % short and wide
+    ylim([0.5 1.5])
+end
+set(gca, 'box', 'off')      % Remove top and right axis lines
+end
