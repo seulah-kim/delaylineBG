18a5796d58a2bf8232e3352c35ac0bc6263af532
Seul Ah Kim <seulah_kim@g.harvard.edu>
2017-09-04 20:06:28 -0400

    PSTH

--- plotBGdelayline.m
+++ plotBGdelayline.m
@@ -6,12 +6,14 @@ plot(Isyn_gp_out')
 subplot(2,1,2)
 plot(Isyn_snr_out')
 
+t_span = 0:0.001:20; 
+
 figure
 subplot(3,1,1)
- plot(t_span,Vm_str(5,1:length(t_span)),'b')
+plot(t_span,Vm_str(5,1:length(t_span)),'b')
 hold on
 %  plot(t_span,Vm_gp(1,1:length(t_span))','b')
- plot(t_span,Vm_snr(1,1:length(t_span)),'r')
+plot(t_span,Vm_snr(1,1:length(t_span)),'r')
 % legend('Vm_{str}','Vm_{gp}','Vm_{snr}')
 legend('Vm_{str} #1','Vm_{snr}')
 ylabel('membrane Voltage (mV)')
@@ -19,7 +21,7 @@ ylabel('membrane Voltage (mV)')
 
 subplot(3,1,2) 
 hold on
-plot(t_span,g_gp2snr(:,1:length(t_span)),'g')
+%plot(t_span,g_gp2snr(:,1:length(t_span)),'g')
 % legend('g_{str2gp}','g_{gp2snr}')
 legend('g_{str2gp}')
 ylabel('conductance (nS)')
